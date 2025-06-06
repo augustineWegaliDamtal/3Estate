f3bfb4409559ed066368b560d5c65e5afdce389f
Augustine Damtal <augustinedamtal@gmail.com>
2025-06-06 09:31:17 +0000

    Signup design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FaSearch} from 'react-icons/fa'
+import {FaSearch,FaHome} from 'react-icons/fa'
 const Header = () => {
   return (
     <div className='p-2'>
       <ul className='flex justify-center gap-4'>
-        <Link to='/home'><li>Home</li></Link>
+        <Link to='/home'><li><FaHome className='text-blue-900 h-6 w-6'/></li></Link>
         <div className='flex relative'>
             <input type='text' className='border border-gray-300 outline-0 p-1.5 rounded-lg' placeholder='search... '/>
-            <FaSearch className='absolute -translate-x-1/2 right-1 my-2.5 '/>
+            <FaSearch className='absolute -translate-x-1/2 right-1 my-2.5 text-gray-600 hover:text-black '/>
         </div>
         <Link to='/about'><li>About</li></Link>
         <Link to='/signin'><li>Signin</li></Link>
