b29cccfcc4654625fb8e78de5b7f8f3f5d968fee
Augustine Damtal <augustinedamtal@gmail.com>
2025-06-06 06:38:18 +0000

    Creating Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
 const App = () => {
   return (
-    <div className='text-red-500'>
-      hellow world
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
